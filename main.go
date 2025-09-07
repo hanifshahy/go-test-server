9c87517e50cf4f8379338d94764edfcc569e2fdb
Mohammad Hanif Shahi <hanifshahy@gmail.com>
2025-09-07 20:29:56 +0430

    server

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Secure Go server is live!"))
-	})
+type response struct {
+	Message string `json:"message"`
+}
 
-	err := http.ListenAndServeTLS(":443", `C:\Users\DELLL\Documents\projects\test\test2\ssl.cert`, `C:\Users\DELLL\Documents\projects\test\test2\domain.key`, nil)
-	if err != nil {
-		log.Fatal("ListenAndServeTLS error: ", err)
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response{Message: "Hello from dadshani-group.com"})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	go func() {
+		log.Println("Starting server on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen error: %v", err)
+		}
+	}()
+
+	// Graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	<-stop
+
+	log.Println("Shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
 }
